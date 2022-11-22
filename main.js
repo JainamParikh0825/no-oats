e7166ade0f8563912c348f2bda2abc7c70d40fcb
Jainam Parikh <jainamparikh0825@gmail.com>
2022-11-22 09:31:56 +0530

    update: add, select, view & delete functionality

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
 import NotesAPI from './NotesAPI.js';
+import NotesView from './NotesView.js';
+
+const app = document.getElementById('app');
+const view = new NotesView(app, {
+    onNoteAdd() {
+        console.log('Note Added!');    
+    },
+    onNoteSelect(id) {
+        console.log('Note Selected: ' + id);
+    },
+    onNoteEdit(newTitle, newBody) {
+        console.log(newTitle, newBody);
+    },
+    onNoteDelete(id) {
+        console.log('Note Deleted: ' + id);
+    }
+});
+
+const notes = NotesAPI.getAllNotes();
+
+view.updateNoteList(notes);
+view.updateActiveNote(notes[0]);
